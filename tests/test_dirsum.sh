040e2937f693a911b78442ea6d6957b78772b45d
brainthra <96686520+brainthra@users.noreply.github.com>
2026-08-08 05:56:36 +0100

    chore: prepare release v1.0.0

--- tests/test_dirsum.sh
+++ tests/test_dirsum.sh
@@ -113,7 +113,7 @@ status=$?
 assert_status "$status" 0 \
     "--version exits successfully"
 
-assert_contains "$output" "dirsum 0.4.0" \
+assert_contains "$output" "dirsum 1.0.0" \
     "--version reports current version"
 
 # Nonexistent directory
